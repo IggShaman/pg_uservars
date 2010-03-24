801a15631170b211959be40ebc19c9f3b1a2401e
Igor Shevchenko <igor.shevchenko@gmail.com>
2010-03-24 14:06:01 +0200

    fixes to various functions, first stable version added test-suite in test/test.pl

--- pg_uservars.c
+++ pg_uservars.c
@@ -15,6 +15,8 @@
 PG_MODULE_MAGIC;
 
 
+#define DEBUG 0
+
 // max expected number of user variables
 #define MAX_EXPECTED_VARS 32
 
@@ -31,25 +33,29 @@ extern Datum pguser_delvar(PG_FUNCTION_ARGS);
 // hash with user keys/values
 //
 static HTAB* ukv_hash = NULL;
-typedef struct {
-    const char* key;
-    Size len;
-} ukv_key_t;
 
+
+//
+// Hash entry structure.
+//
+// Contains a copy of a key and a copy of data. Both strings are allocated in
+// TopMemoryContext and freed when key is removed via pguser_delvar(..).
+//
 typedef struct {
+    char* key;
+    Size key_sz;
     char* val;
-    Size len;
-} ukv_value_t;
+    Size val_sz;
+} ukv_entry_t;
 
 
 //
 // hash accessors
 //
 static void ukv_hash_init ( void );
-static ukv_value_t* ukv_hash_get ( const char* key, Size len );
+static ukv_entry_t* ukv_hash_get ( const char* key, Size len );
 static void ukv_hash_set ( const char* key, Size key_len, const char* val, Size val_len );
 static void ukv_hash_del ( const char* key, Size len );
-
 
 #define PG_CSTR_GET_TEXT(cstrp) DatumGetTextP(DirectFunctionCall1(textin, CStringGetDatum(cstrp)))
 #define PG_TEXT_GET_CSTR(textp) DatumGetCString(DirectFunctionCall1(textout, PointerGetDatum(textp)))
@@ -70,7 +76,9 @@ Datum pguser_setvar ( PG_FUNCTION_ARGS ) {
     key = PG_TEXT_GET_CSTR(PG_GETARG_TEXT_P(0));
     val = PG_TEXT_GET_CSTR(PG_GETARG_TEXT_P(1));
     
-    //elog ( INFO, "set \"%s\" => \"%s\"", key, val );
+    #if DEBUG
+    elog ( INFO, "pguser_setvar: set \"%s\" => \"%s\"", key, val );
+    #endif
     ukv_hash_set ( key, strlen(key), val, strlen(val) );
     
     PG_RETURN_BOOL(true);
@@ -84,19 +92,28 @@ PG_FUNCTION_INFO_V1(pguser_getvar);
 Datum pguser_getvar ( PG_FUNCTION_ARGS ) {
     
     char* key;
-    ukv_value_t* val;
+    ukv_entry_t* ent;
     
     if ( PG_ARGISNULL(0) )
     	PG_RETURN_NULL();
     
     key = PG_TEXT_GET_CSTR(PG_GETARG_TEXT_P(0));
     
-    //elog ( INFO, "get \"%s\"", key );
-    val = ukv_hash_get ( key, strlen(key) );
-    if ( val )
-	PG_RETURN_TEXT_P(PG_CSTR_GET_TEXT(val->val));
-    else
-	PG_RETURN_NULL();
+    #if DEBUG
+    elog ( INFO, "pguser_getvar: key=\"%s\"", key );
+    #endif
+    ent = ukv_hash_get ( key, strlen(key) );
+    if ( ent ) {
+	#if DEBUG
+	elog ( INFO, "pguser_getvar: key=\"%s\" return \"%s\"", ent->key, ent->val );
+	#endif
+	PG_RETURN_TEXT_P(PG_CSTR_GET_TEXT(ent->val));
+    }
+    
+    #if DEBUG
+    elog ( INFO, "pguser_getvar: key=\"%s\" return NULL", key );
+    #endif
+    PG_RETURN_NULL();
 }
 
 
@@ -117,125 +134,190 @@ Datum pguser_delvar ( PG_FUNCTION_ARGS ) {
 }
 
 
-static ukv_value_t* ukv_hash_get ( const char* name, Size name_len ) {
+static ukv_entry_t* ukv_hash_get ( const char* name, Size name_sz ) {
     
-    ukv_key_t key;
-    ukv_value_t* val;
+    ukv_entry_t key;
+    ukv_entry_t* ent;
     
     if ( !ukv_hash )
     	return NULL;
     
-    key.key = name;
-    key.len = name_len;
+    key.key = (char*)name;
+    key.key_sz = name_sz;
     
-    val = (ukv_value_t*)hash_search ( ukv_hash, &key, HASH_FIND, NULL );
-    //elog ( INFO, "name=\"%s\" size=%lu got value=%lu", name, name_len, (Size)val );
-    return val;
+    ent = (ukv_entry_t*)hash_search ( ukv_hash, &key, HASH_FIND, NULL );
+    #if DEBUG
+    elog ( INFO, "hash_get: key=\"%s\" size=%lu got ptr=%lu", name, name_sz, (Size)ent );
+    #endif
+    return ent;
 }
 
 
+//
+// Hasher function for key part of ukv_entry_t.
+//
 static uint32 ukv_key_hash ( const void* key, Size keysize ) {
-    int32_t hv = string_hash ( ((ukv_key_t*)key)->key, ((ukv_key_t*)key)->len );
-    //elog ( INFO, "computing hash for key=\"%s\" size=%d hash=%d", ((ukv_key_t*)key)->key, keysize, hv );
+    int32_t hv = string_hash ( ((ukv_entry_t*)key)->key, ((ukv_entry_t*)key)->key_sz );
+    #if DEBUG
+    elog ( INFO, "computing hash for key=\"%s\" size=%lu hash=%d", ((ukv_entry_t*)key)->key, keysize, hv );
+    #endif
     return hv;
 }
 
 
-// key comparison function
-static int ukv_key_match ( const void* key1, const void* key2, Size keysize ) {
+//
+// Key comparison function for key part of ukv_entry_t
+//
+static int ukv_key_match ( const void* ent1, const void* ent2, Size keysize ) {
     int match;
     
-    Assert ( keysize == sizeof(ukv_key_t) );
+    Assert ( keysize == sizeof(ukv_entry_t) );
     
     do {
-	if ( ((ukv_key_t*)key1)->len != ((ukv_key_t*)key2)->len ) {
+	if ( ((ukv_entry_t*)ent1)->key_sz != ((ukv_entry_t*)ent2)->key_sz ) {
 	    match = false;
 	    break;
 	}
 	
-	match = (0 == memcmp ( ((ukv_key_t*)key1)->key, ((ukv_key_t*)key2)->key, ((ukv_key_t*)key1)->len ));
+	match = memcmp ( ((ukv_entry_t*)ent1)->key, ((ukv_entry_t*)ent2)->key, ((ukv_entry_t*)ent1)->key_sz );
     } while ( false );
     
-    //elog ( INFO, "\"%s\" match \"%s\" = %s",
-    //((ukv_key_t*)key1)->key,
-    //((ukv_key_t*)key2)->key,
-    //match ? "true" : "false" );
+    #if DEBUG
+    elog ( INFO, "\"%s\" match \"%s\" = %s",
+	   ((ukv_entry_t*)ent1)->key,
+	   ((ukv_entry_t*)ent2)->key,
+	   0 == match ? "true" : "false" );
+    #endif
     
     return match;
 }
 
 
+//
+// Create copy of a memory region in TopMemoryContext
+//
+static char* p_memdup_top ( const void* ptr, Size sz ) {
+    char* p2 = MemoryContextAlloc ( TopMemoryContext, sz+1 );
+    if ( !p2 )
+	elog ( ERROR, "out of memory" );
+    
+    memcpy ( p2, ptr, sz );
+    p2[sz] = 0; // final \0, for strings
+    return p2;
+}
+
+
+//
+// Key copy: dup key part, leave value NULL.
+//
+// This callback is called by hash_search(..) when HASH_ENTER operation is used,
+// and a new key is about to be insterted to our hash.
+//
+static void* ukv_key_copy ( void* dest, const void* src, Size sz ) {
+    ((ukv_entry_t*)dest)->key = p_memdup_top ( ((ukv_entry_t*)src)->key, ((ukv_entry_t*)src)->key_sz );
+    ((ukv_entry_t*)dest)->key_sz = ((ukv_entry_t*)src)->key_sz;
+    ((ukv_entry_t*)dest)->val = NULL;
+    ((ukv_entry_t*)dest)->val_sz = 0;
+    
+    #if DEBUG
+    elog ( INFO, "copied key=\"%s\" sz=%lu", ((ukv_entry_t*)dest)->key, ((ukv_entry_t*)dest)->key_sz );
+    #endif
+    
+    return dest;
+}
+
+
+//
+// ukv_hash initialization.
+//
 static void ukv_hash_init ( void ) {
     HASHCTL ctl;
     
-    ctl.keysize = sizeof(ukv_key_t);
-    ctl.entrysize = sizeof(ukv_value_t);
+    ctl.keysize = sizeof(ukv_entry_t);
+    ctl.entrysize = sizeof(ukv_entry_t);
     ctl.hash = ukv_key_hash;
     ctl.match = ukv_key_match;
+    ctl.keycopy = ukv_key_copy;
     
     ukv_hash = hash_create ( "user key/value hash", MAX_EXPECTED_VARS, &ctl,
-			     HASH_FUNCTION|HASH_ELEM|HASH_COMPARE );
+			     HASH_FUNCTION|HASH_ELEM|HASH_COMPARE|HASH_KEYCOPY );
     
     if ( NULL == ukv_hash )
 	ereport ( ERROR, (errcode(ERRCODE_OUT_OF_MEMORY), errmsg("out of memory")) );
 }
 
 
-static char* p_memdup_top ( const void* ptr, Size sz ) {
-    void* p2 = MemoryContextAlloc ( TopMemoryContext, sz );
-    if ( !p2 )
-	elog ( ERROR, "out of memory" );
+//
+// Adds new value to hash, or sets a new value for pre-existing key.
+//
+static void ukv_hash_set ( const char* key_name, Size key_sz, const char* new_val, Size new_val_sz ) {
     
-    memcpy ( p2, ptr, sz );
-    return p2;
-}
-
-
-static void ukv_hash_set ( const char* name, Size name_len, const char* new_val, Size new_val_len ) {
-    
-    ukv_key_t key;
-    ukv_value_t* val;
+    ukv_entry_t key;
+    ukv_entry_t* ent;
     bool found;
-    
-    //elog ( INFO, "setting variable name=\"%s\" size=%d value=\"%s\" size=%d", name, name_len, new_val, new_val_len );
     
     if ( !ukv_hash )
 	ukv_hash_init();
     
-    key.key = name;
-    key.len = name_len;
+    key.key = (char*)key_name;
+    key.key_sz = key_sz;
+    key.val = NULL;
+    key.val_sz = 0;
     
-    val = (ukv_value_t*)hash_search ( ukv_hash, &key, HASH_ENTER, &found );
+    ent = (ukv_entry_t*)hash_search ( ukv_hash, &key, HASH_ENTER, &found );
     
-    if ( NULL == val )
+    if ( NULL == ent )
 	ereport ( ERROR, (errcode(ERRCODE_OUT_OF_MEMORY), errmsg("out of memory")));
     
     if ( found ) {
-	if ( val->val )
-	    pfree ( val->val );
+	#if DEBUG
+	elog ( INFO, "key \"%s\" found", key_name );
+	#endif
+	if ( ent->val )
+	    pfree ( ent->val );
 	
-	//ereport(ERROR, (errcode(ERRCODE_DUPLICATE_OBJECT), errmsg("duplicate string name")));
+    } else {
+	#if DEBUG
+	elog ( INFO, "key \"%s\" not found, inserting", key_name );
+	#endif
     }
     
-    val->val = p_memdup_top ( new_val, new_val_len );
-    //elog ( INFO, "value set to \"%s\"", val->val );
+    // key and key_sz are copied via ukv_key_copy
+    //ent->key = p_memdup_top ( key_name, key_sz );
+    //ent->key_sz = key_sz;
+    ent->val = p_memdup_top ( new_val, new_val_sz );
+    ent->val_sz = new_val_sz;
+    
+    #if DEBUG
+    elog ( INFO, "ukv_hash_set: key=\"%s\" value set to \"%s\" len=%lu; new_val=\"%s\" new_val_len=%lu",
+	   key_name, ent->val, ent->val_sz, new_val, new_val_sz );
+    #endif
 }
 
 
-static void ukv_hash_del ( const char* name, Size name_len ) {
+//
+// Removes key/value pair from ukv_hash and frees memory.
+//
+static void ukv_hash_del ( const char* name, Size name_sz ) {
     
-    ukv_key_t key;
-    ukv_value_t* val;
+    ukv_entry_t key;
+    ukv_entry_t* ent;
     bool found;
     
     if ( !ukv_hash )
     	return; // no hash => no keys
     
-    key.key = name;
-    key.len = name_len;
+    key.key = (char*)name;
+    key.key_sz = name_sz;
     
-    val = (ukv_value_t*)hash_search ( ukv_hash, &key, HASH_REMOVE, &found );
+    ent = (ukv_entry_t*)hash_search ( ukv_hash, &key, HASH_REMOVE, &found );
     
-    if ( val )
-    	pfree ( val->val );
+    if ( ent ) {
+    	pfree ( ent->key );
+    	pfree ( ent->val );
+    }
+    
+    #if DEBUG
+    elog ( INFO, "ukv_hash_del: key=\"%s\"", name );
+    #endif
 }
---
+++ test/test.pl
@@ -0,0 +1,149 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+use IO::Handle;
+autoflush STDOUT 1;
+
+use DBI;
+use DBD::Pg;
+
+my $dbh = eval { DBI->connect ( 'dbi:Pg:dbname=test1', '', '',
+                                { RaiseError => 1,
+                                  AutoCommit => 1,
+                                  #PrintError => 1,
+                                } ); };
+die "ERROR: could not connect to db: $@" if $@;
+
+
+# hash with vars
+# undef value means variable was destroyed
+my $vars = {};
+
+sub getvar {
+    my $key = $_[0];
+    
+    my ($val) = $dbh->selectrow_array ( 'select pguser_getvar(?)', undef, $key );
+    #print "getvar \"$key\" => ", (defined $val ? '"' . $val . '"' : '<undef>'), "\n";
+    
+    if ( defined $val ) {
+	die "\$vars: key=\"$key\" no value for key=\"$key\"\n" unless exists $vars->{$key} and defined $vars->{$key};
+	die "\$vars: key=\"$key\": value \"$vars->{$key}\" != db=\"$val\"\n" unless $vars->{$key} eq $val;
+    } else {
+	die "\$vars: key=\"$key\": have value=\"$vars->{$key}\", db hasn't\n" if exists $vars->{$key} and defined $vars->{$key};
+    }
+    
+    return $val;
+}
+
+sub setvar {
+    my ($key, $val) = @_;
+    $dbh->selectrow_array ( 'select pguser_setvar(?,?)', undef, $key, $val );
+    #print "setvar \"$key\" => ", (defined $val ? '"' . $val . '"' : '<undef>'), "\n";
+    $vars->{$key} = $val;
+}
+
+
+sub delvar {
+    my ($key) = @_;
+    $dbh->selectrow_array ( 'select pguser_delvar(?)', undef, $key );
+    #print "delvar \"$key\"\n";
+    $vars->{$key} = undef;
+}
+
+sub randstr {
+    my $sz = 3 + int rand($_[0] // 3);
+    my @rv;
+    while ( $sz-- ) {
+	push @rv, chr(ord('a') + int rand(26));
+    }
+    my $rv = join ( '', @rv );
+    #print "randstr \"$rv\"\n";
+    return $rv;
+}
+
+my @rand_names;
+for ( 1..1000 ) {
+    push @rand_names, randstr();
+}
+my $rand_names_nr = scalar(@rand_names);
+
+sub randname {
+    return $rand_names[int rand($rand_names_nr)];
+}
+
+
+my $nr = 0;
+print "Progress: [";
+while ( $nr++ < 1000000 ) {
+    
+    print '.' if $nr % 1000 == 0;
+    print ' ', $nr, ' ' if $nr % 100000 == 0;
+    
+    my $op = int rand(3);
+    
+    if ( 0 == $op ) {      # add random value
+	setvar ( randname(), randstr(10240) );
+	
+    } elsif ( 1 == $op ) { # change existing value
+	my @k = keys %$vars;
+	next unless @k;
+	my $i = int(rand(scalar(@k)));
+	my $key = $k[$i];
+	unless ( defined $key and length $key ) {
+	    die "UNDEFINED KEY! rand i=$i len=", scalar(@k), "\n";
+	}
+	setvar ( $key, randstr() );
+	
+    } elsif ( 2 == $op ) { # remove existing value
+	my @k = keys %$vars;
+	next unless @k;
+	my $i = int(rand(scalar(@k)));
+	my $key = $k[$i];
+	unless ( defined $key and length $key ) {
+	    die "UNDEFINED KEY! rand i=$i len=", scalar(@k), "\n";
+	}
+	
+	delvar ( $key ) if defined $key;
+	
+    } elsif ( 3 == $op ) { # del random value
+	delvar ( randname() );
+    }
+}
+print "]\n";
+
+#
+# check all
+#
+my $tot_nr = 0;
+my $set_nr = 0;
+while ( my ($k, $v) = each %$vars ) {
+    #note: getvar does all checks
+    my $v = getvar ( $k );
+    
+    $tot_nr++;
+    $set_nr++ if defined $v;
+}
+
+delvar ( $_ ) foreach keys %$vars;
+
+print "total entr(ies): $tot_nr\n";
+print "  set entr(ies): $set_nr\n";
+
+#
+# print memory usage while still connected
+#
+print `ps auxww|grep postgres`;
+
+#
+# disconnect from db and let it free up some memory
+#
+$dbh->disconnect();
+sleep 2;
+
+#
+# print memory usage after disconnect
+#
+print `ps auxww|grep postgres`;
+
+1;
